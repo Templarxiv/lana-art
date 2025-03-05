cf3b9c3999f5050291e3ed4cf5dd2d27d5aa0c06
MTemplarrr <sgv@agentarsenal.ru>
2025-03-06 02:19:38 +0300

    init

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Contacts from '../components/Contacts.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  mode: 'history',
   routes: [
     {
       path: '/',
